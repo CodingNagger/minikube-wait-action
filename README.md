c79cc5d59ad997597000654fa598894a1ea90be8
CodingNagger <contact@iamnguele.com>
2019-11-05 15:46:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Minikube wait action
+
+This action waits until all your pods a VM-free Kubernetes cluster using Minikube.
+
+## Pre-requisites
+
+Your Actions workflow has a kubernetes setup. You may use [minikube-setup-action](https://github.com/marketplace/actions/minikube-setup-action) for that.
+
+## Inputs
+
+### `max-retry-attempts`
+
+**Optional** Maximum of times the check can be carried on. Default `10`.
+
+### `retry-delay`
+
+**Optional** Time to wait (in seconds) before attempting the check after a failure. Default `30`.
+
+## Example usage
+
+```yaml
+name: "Minikube workflow"
+on: [push]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@master
+    - name: Wait Minikube
+      uses: CodingNagger/minikube-setup-action@v1.0.2
+    - name: Launch Minikube
+      run: eval ${{ steps.minikube.outputs.launcher }}
+    - name: Install Nginx pod
+      run: kubectl apply -f https://k8s.io/examples/application/deployment.yaml
+    - name: Wait for pods
+      uses: CodingNagger/minikube-wait-action@v1.0.0
+    - name: Check pods
+      run: |
+        kubectl get pods
+```
---
+++ src/index.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core');
+const { spawnSync } = require('child_process');
+
+const successMessage = 'No resources found.';
+const maxRetryAttempts = parseInt(`${core.getInput('max-retry-attempts')}`);
+const retryDelay = parseInt(`${core.getInput('retry-delay')}`);
+
+function checkPodsAreUp() {
+    var lastCommandRunning = spawnSync('kubectl', ['get', 'pods','--field-selector=status.phase!=Running,status.phase!=Completed']);
+    var output = lastCommandRunning.stdout.toString();
+    return {
+        success: output == successMessage,
+        message: output,
+    };
+}
+
+function wait(seconds) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, seconds*1000);
+}
+
+if (maxRetryAttempts <= 0 || retryDelay <= 0) {
+    core.setFailed('No negative config values!');
+    return;
+}
+
+try {
+    console.log('Check pods are up...');
+    var result = checkPodsAreUp();
+    var attemptsCount = 0;
+    
+    while(!result.success && attemptsCount < maxRetryAttempts) {
+        console.log(`Waiting ${retryDelay} seconds`);
+        attemptsCount++;
+        wait(retryDelay);
+        console.log(`Retry...${attemptsCount}`);
+        result = checkPodsAreUp();
+        console.log(result.message);
+    }
+
+    console.log(`All pods are up!!!`);
+} catch (error) {
+    core.setFailed(error.message);
+}
